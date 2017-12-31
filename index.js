8ed5dad08843cfdb173f26c01515ad08483f336f
Constance Goh <constgoh@gmail.com>
2017-12-31 04:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue (object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  obj[key] = value
+  var obj = { prop: 1, }
   return obj
 }
